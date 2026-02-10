14937672d57896bf6b7b2a17a6ea9a5ca666c1c6
Nishtman <nishtman_vo>
2026-02-10 13:23:35 +0330

    push third

---
+++ third.py
@@ -0,0 +1,22 @@
+import requests
+#this comment added by remote
+# this line to check version control added in remote
+#*********************
+url = "https://jsonplaceholder.typicode.com/todos/1"
+
+response = requests.get(url)
+if response.status_code == 200 :
+    data = response.json()
+    print("data received")
+    print(data)
+else :
+
+    print("error in receiving data" , response.status_code)
+
+    print("error in receiving data" , response.status_code)
+
+    print("error in receiving data" , response.status_code)
+
+
+    # added by local automation
+
